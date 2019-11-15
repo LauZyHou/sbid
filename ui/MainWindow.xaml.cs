00e8577812a88887fdc7211cb6b985ad15b00b6c
LauZyHou <java233@foxmail.com>
2019-11-15 20:16:35 +0800

    快捷按纽栏雏形 & 从图菜单新建选项卡

--- ui/MainWindow.xaml.cs
+++ ui/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace sbid
     public partial class MainWindow : Window
     {
         UserVM _uvm;
+        private int tabIndex = 0; // 在新建tab(实际上是新建图)时用于区分默认的新建名称
 
         public MainWindow()
         {
@@ -36,6 +37,43 @@ namespace sbid
             _uvm.CompanyName = "ecnu";
             Console.WriteLine("okk");
             MessageBox.Show("123");
+        }
+
+        //[菜单]图 > SysML图 > 测试用
+        private void MenuItem_Click_Test(object sender, RoutedEventArgs e)
+        {
+            //创建新的选项卡
+            TabItem ti = new TabItem();
+            ti.Header = "xx图" + (tabIndex + 1);
+            TabControl1.Items.Add(ti);
+            //为新选项卡加入有背景色的Grid
+            Color color = (Color)ColorConverter.ConvertFromString("LightBlue");
+            SolidColorBrush brush = new SolidColorBrush(color);
+            Grid gd = new Grid();
+            gd.Background = brush;
+            ti.Content = gd;
+
+            //每造一个新窗口便默认突出显示为新窗口
+            TabControl1.SelectedIndex = tabIndex + 1;
+            tabIndex++;
+            //双击每个选项卡触发的事件
+            ti.MouseDoubleClick += TabItem_DoubleClick;
+        }
+
+        //[菜单]文件 > 退出
+        private void MenuItem_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // ----------------------------------------------------------------
+
+        //双击选项卡
+        private void TabItem_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //关闭选项卡
+            TabItem s = (TabItem)sender;
+            TabControl1.Items.Remove(s);
         }
     }
 }
