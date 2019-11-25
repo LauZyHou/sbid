d84beb069c754a434d3e40d78c4d1d4e8a93a562
LauZyHou <java233@foxmail.com>
2019-11-25 22:30:28 +0800

    TabItem通过容纳TabControl实现嵌套

---
+++ UserControl/GraphButtonStack/GraphButtonStack.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sbid.UserControl
+{
+    /// <summary>
+    /// GraphButtonStack.xaml 的交互逻辑
+    /// </summary>
+    public partial class GraphButtonStack : System.Windows.Controls.UserControl
+    {
+        public GraphButtonStack()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //todo 1 让父窗体bottomTextBox显示内容
+            Window father = Window.GetWindow(this);
+            father.Title = "123";
+        }
+    }
+}
--- ui/MainWindow.xaml.cs
+++ ui/MainWindow.xaml.cs
@@ -77,12 +77,75 @@ namespace sbid
 
         // ----------------------------------------------------------------
 
-        //双击选项卡
+        //双击大面板上的选项卡
         private void TabItem_DoubleClick(object sender, MouseButtonEventArgs e)
         {
             //关闭选项卡
             TabItem s = (TabItem)sender;
             TabControl1.Items.Remove(s);
         }
+
+        //点击菜单[图]之下的菜单项时触发
+        private void MenuItem_Click_Graph(object sender, RoutedEventArgs e)
+        {
+            MenuItem mit = (MenuItem)sender;
+            if (mit != null)
+            {
+                bottomTextBlock.Text = "当前选择：" + mit.Header.ToString();
+            }
+        }
+
+        //菜单 > 文件 > 新模型
+        private void MenuItem_Click_NewModel(object sender, RoutedEventArgs e)
+        {
+            //新模型的选项卡，添加到总的选项卡容器
+            TabItem ti = new TabItem()
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                Header = "模型" + (tabIndex + 1)
+            };
+            TabControl1.Items.Add(ti);
+            //选项卡的内容又是选项卡容器，在其中添加一个默认的概览选项卡
+            TabControl tc = new TabControl()
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch
+            };
+            ti.Content = tc;
+            TabItem overViewTab = new TabItem()
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                Header = "概览"
+            };
+            tc.Items.Add(overViewTab);
+
+            //每造一个新窗口便默认突出显示为新窗口
+            TabControl1.SelectedIndex = tabIndex;
+            tabIndex++;
+            //双击每个选项卡触发的事件
+            ti.MouseDoubleClick += TabItem_DoubleClick;
+        }
+
+        //菜单 > 模型 > 添加状态机(State Machine)
+        private void MenuItem_Click_Add_State_Machine(object sender, RoutedEventArgs e)
+        {
+            //获取当前活动的Model选项卡下的TabControl
+            TabItem tabItem = TabControl1.Items.GetItemAt(tabIndex - 1) as TabItem;
+            TabControl tabControl = tabItem.Content as TabControl;
+            //在其中添加状态机选项卡
+            TabItem ti = new TabItem()
+            {
+                VerticalAlignment = VerticalAlignment.Stretch,
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                Header = "状态机1"
+            };
+            //ti.MouseDoubleClick += ???
+            tabControl.Items.Add(ti);
+        }
+        //菜单 > 模型 > 添加自定义类型(User Type)
+        //菜单 > 模型 > 添加公理(Axiom)
+
     }
 }
