ef561ef4499b1c043638c537bc4f23a13a463396
LauZyHou <java233@foxmail.com>
2019-11-26 23:22:36 +0800

    添加 功能模板->模型内容

---
+++ UserControl/FuncPanel/ModelFuncPanel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sbid.UserControl
+{
+    /// <summary>
+    /// ModelFuncPanel.xaml 的交互逻辑
+    /// 不应TabItem拿出来设计的，这里就是基于这个考虑，把TabControl里的内容抽成UserControl
+    /// </summary>
+    public partial class ModelFuncPanel : System.Windows.Controls.UserControl
+    {
+        public ModelFuncPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ui/MainWindow.xaml.cs
+++ ui/MainWindow.xaml.cs
@@ -22,13 +22,12 @@ namespace sbid
     public partial class MainWindow : Window
     {
         UserVM _uvm;
-        private int tabIndex = 0; // 在新建tab(实际上是新建图)时用于区分默认的新建名称
+        private int tabId = 0; // 在新建模型选项卡时用于区分的自增数字
+        private int tabNum = 0; //记录实际存在的模型选项卡的数目
 
         public MainWindow()
         {
             InitializeComponent();
-            //this.WindowState = System.Windows.WindowState.Maximized;
-            //this.WindowStyle = System.Windows.WindowStyle.None;
             _uvm = base.DataContext as UserVM;
         }
 
@@ -43,30 +42,7 @@ namespace sbid
         //[菜单]图 > SysML图 > 测试用
         private void MenuItem_Click_Test(object sender, RoutedEventArgs e)
         {
-            /*
-            //创建新的选项卡
-            TabItem ti = new TabItem();
-            ti.Header = "xx图" + (tabIndex + 1);
-            TabControl1.Items.Add(ti);
-            //为新选项卡加入有背景色的Grid
-            Color color = (Color)ColorConverter.ConvertFromString("LightBlue");
-            SolidColorBrush brush = new SolidColorBrush(color);
-            Grid gd = new Grid();
-            gd.Background = brush;
-            ti.Content = gd;
-            */
-            TabItem ti = new TabItem();
-            ti.VerticalAlignment = VerticalAlignment.Stretch;
-            ti.HorizontalAlignment = HorizontalAlignment.Stretch;
-            ti.Header = "xx图" + (tabIndex + 1);
-            TabControl1.Items.Add(ti);
-            ti.Content = new GraphPanel();
-
-            //每造一个新窗口便默认突出显示为新窗口
-            TabControl1.SelectedIndex = tabIndex + 1;
-            tabIndex++;
-            //双击每个选项卡触发的事件
-            ti.MouseDoubleClick += TabItem_DoubleClick;
+           
         }
 
         //[菜单]文件 > 退出
@@ -77,12 +53,14 @@ namespace sbid
 
         // ----------------------------------------------------------------
 
-        //双击大面板上的选项卡
+        //[双击]模型选项卡的标签
         private void TabItem_DoubleClick(object sender, MouseButtonEventArgs e)
         {
-            //关闭选项卡
+            //移除选项卡
             TabItem s = (TabItem)sender;
-            TabControl1.Items.Remove(s);
+            mainTabControl.Items.Remove(s);
+            //维护总选项卡数目
+            this.tabNum--;
         }
 
         //点击菜单[图]之下的菜单项时触发
@@ -98,52 +76,25 @@ namespace sbid
         //菜单 > 文件 > 新模型
         private void MenuItem_Click_NewModel(object sender, RoutedEventArgs e)
         {
-            //新模型的选项卡，添加到总的选项卡容器
-            TabItem ti = new TabItem()
+            //创建TabItem并包含新的模型控件
+            TabItem tabItem = new TabItem
             {
-                VerticalAlignment = VerticalAlignment.Stretch,
-                HorizontalAlignment = HorizontalAlignment.Stretch,
-                Header = "模型" + (tabIndex + 1)
+                Content = new ModelFuncPanel()
             };
-            TabControl1.Items.Add(ti);
-            //选项卡的内容又是选项卡容器，在其中添加一个默认的概览选项卡
-            TabControl tc = new TabControl()
-            {
-                VerticalAlignment = VerticalAlignment.Stretch,
-                HorizontalAlignment = HorizontalAlignment.Stretch
-            };
-            ti.Content = tc;
-            TabItem overViewTab = new TabItem()
-            {
-                VerticalAlignment = VerticalAlignment.Stretch,
-                HorizontalAlignment = HorizontalAlignment.Stretch,
-                Header = "概览"
-            };
-            tc.Items.Add(overViewTab);
+            mainTabControl.Items.Add(tabItem);
 
-            //每造一个新窗口便默认突出显示为新窗口
-            TabControl1.SelectedIndex = tabIndex;
-            tabIndex++;
-            //双击每个选项卡触发的事件
-            ti.MouseDoubleClick += TabItem_DoubleClick;
+            //每造一个新窗口便默认突出显示为活动窗口
+            mainTabControl.SelectedIndex = this.tabNum;
+            this.tabId++;
+            this.tabNum++;
+
+            //[新模型的TabItem的参数设定]
+            tabItem.MouseDoubleClick += TabItem_DoubleClick;
+            tabItem.Header = "模型" + this.tabId.ToString();
         }
 
+        //todo 放到模型控件里面去做
         //菜单 > 模型 > 添加状态机(State Machine)
-        private void MenuItem_Click_Add_State_Machine(object sender, RoutedEventArgs e)
-        {
-            //获取当前活动的Model选项卡下的TabControl
-            TabItem tabItem = TabControl1.Items.GetItemAt(tabIndex - 1) as TabItem;
-            TabControl tabControl = tabItem.Content as TabControl;
-            //在其中添加状态机选项卡
-            TabItem ti = new TabItem()
-            {
-                VerticalAlignment = VerticalAlignment.Stretch,
-                HorizontalAlignment = HorizontalAlignment.Stretch,
-                Header = "状态机1"
-            };
-            //ti.MouseDoubleClick += ???
-            tabControl.Items.Add(ti);
-        }
         //菜单 > 模型 > 添加自定义类型(User Type)
         //菜单 > 模型 > 添加公理(Axiom)
 
